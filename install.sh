720f539ce766b0d1feb83a822189fe3eddd76790
kleidione <48599586+kleidione@users.noreply.github.com>
2020-07-26 18:58:59 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+# Instala os pacotes básicos do archlinux:
+pacstrap /mnt base base-devel linux linux-firmware grub nano dhcpcd
+
+# Gera a fstab:
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+# Copia script post-install e pacman.conf >> /mnt:
+cp pacman.conf /mnt/etc/ 
+cp post-install.sh /mnt
+
+# Entra em chroot >> /mnt e executa post-install:
+arch-chroot /mnt sh post-install.sh
---
+++ post-install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash 
+
+# Atualiza repositórios:
+pacman -Syu
+
+# Define o idioma padrão do sistema:
+sed -i '/en_US.U/s/^/#/' /etc/locale.gen ; sed -i '/pt_BR/,+1 s/^#//' /etc/locale.gen ; echo LANG=pt_BR.UTF-8 > /etc/locale.conf ; locale-gen ; export LANG=pt_BR.UTF-8
+
+# Define o idioma padrão do teclado:
+loadkeys br-abnt ; echo -e "KEYMAP=br-abnt2\nFONT=Lat2-Terminus16\nFONT_MAP=" > /etc/vconsole.conf ; mkdir -p /etc/X11/xorg.conf.d ; echo -e 'Section "InputClass"\nIdentifier "Keyboard Defaults"\nMatchIsKeyboard "yes"\nOption "XkbLayout" "br"\nEndSection' | tee /etc/X11/xorg.conf.d/01-keyboard-layout.conf
+
+# Define a hora local:
+ln -sf /usr/share/zoneinfo/America/Belem /etc/localtime
+sed -i '/NTP/,+1 s/^#//' /etc/systemd/timesyncd.conf ; systemctl enable systemd-timesyncd ; clear
+
+# Habilita o uso do sudo a usuários do grupo wheel:
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers
+
+# Define senha para o usuário root:
+clear ; echo " "  ; echo "Senha para o usuário ROOT" ;  passwd ; sleep 2
+
+# Cria e define uma senha para o usuário padrão:
+clear ; echo "Digite o nome de usuário:" ; read usrname
+useradd -m -g users -G wheel,storage,power -s /bin/bash $usrname ; sleep 1 ; clear ; echo "Senha do usuário" ; passwd $usrname ; chsh -s /bin/zsh $usrname
+clear ; sleep 2
+
+# Define o nome do host (maquina):
+echo "archlinux" > /etc/hostname
+
+# Instala o Grub:
+echo "Instalando o grub em /dev/sda..." ; sleep 2
+grub-install --recheck /dev/sda ; grub-mkconfig -o /boot/grub/grub.cfg 
+
+# Instala o xorg + extras:
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xrandr nvidia nvidia-settings pulseaudio ttf-dejavu ttf-liberation
+
+# Instala o i3-gapps + extras:
+pacman -S --noconfirm i3-gaps i3status dmenu xterm hsetroot xcompmgr
+
+# Instala aplicações:
+pacman -S --noconfirm wget git curl p7zip ntfs-3g hdparm gvfs gvfs-mtp xdg-user-dirs xdg-utils xfce4-terminal xfce4-screenshooter thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler xarchiver mousepad ristretto lxappearance firefox telegram-desktop
+
+# Gera e escreve o arquivo .xinitrc:
+echo 'startx' >> /etc/skel/.bash_profile ; echo 'exec i3' >> /etc/skel/.xinitrc
+
+# Cria grupo de pastas padrão ao usuário:
+cd /etc/skel/ 
+mkdir -p "Desktop" "Documentos" "Downloads" "Imagens" "Modelos" "Música" "Público" "Vídeos" "Wallpapers" ".icons" ".themes" 
+touch "/etc/skel/Modelos/Playlist.m3u"
+
+# Habilita o dhcpcd durante o boot:
+systemctl enable dhcpcd
+
+clear ; echo " " ; echo "Script finalizado. Pressione uma tecla para encerrar." ; read tecla
