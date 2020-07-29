6237acfb7261726e533bbf545ff34598cced54da
kleidione <48599586+kleidione@users.noreply.github.com>
2020-07-28 21:20:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 # Instala os pacotes básicos do archlinux:
-pacstrap /mnt base base-devel linux linux-firmware grub nano dhcpcd
+pacstrap /mnt base base-devel linux linux-firmware grub nano dhcpcd net-tools
 
 # Gera a fstab:
 genfstab -U -p /mnt >> /mnt/etc/fstab
